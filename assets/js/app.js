5e58135164fb1d3648c2e8cbb10073e242ea0bc3
dylan12manley <dylan.manley@gmail.com>
2020-11-03 12:46:11 -0800

    follow walkthruogh

--- assets/js/app.js
+++ assets/js/app.js
@@ -52,7 +52,9 @@ const pageClasses = {
     wishlists: () => import('./theme/wishlist'),
 };
 
-const customClasses = {};
+const customClasses = {
+    'pages\\custom\\category\\react-demo': () => import('./theme/custom/react-demo'),
+};
 
 /**
  * This function gets added to the global window and then called
